8a943a4b49b933b4079dfcb697f3a992b7208d45
Franzé Jr <franzejr@gmail.com>
2017-08-29 18:56:34 -0300

    Add output file and annotation for main method

--- src/Main.elm
+++ src/Main.elm
@@ -3,10 +3,12 @@ module Main exposing (main)
 import App
 import View
 import Update
+import Model
 import Navigation
 import Msg exposing (Msg(UrlChange))
 
 
+main : Program Never Model.Model Msg
 main =
     Navigation.program UrlChange
         { view = View.view
